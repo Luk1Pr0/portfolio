8ab6469c09b3426dc5b4ae2f007d5c1055aec067
Luk1Pr0 <lpytel16@gmail.com>
2020-10-25 22:49:22 +0100

    Added functionality to the form, and to the button

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const scrollButton = document.querySelector("#scroll-button");
 const navButton = document.querySelector("#nav-button");
 const aboutSection = document.querySelector("#about");
 const navMenu = document.querySelector("#nav-menu");
+const navLinks = document.querySelectorAll("nav ul li");
+const contactForm = document.querySelector("#contact-form");
+const submitFormBtn = document.querySelector("#send-button");
 
 // Scroll to about section
 const scrollToAbout = () => {
@@ -10,20 +13,67 @@ const scrollToAbout = () => {
     aboutSection.scrollIntoView(true);
 }
 
-const toggleNav = (e) => {
+const toggleNav = () => {
     navMenu.classList.toggle("nav-active");
-    navButton.classList.toggle("active-bar")
-    // if (navMenu.classList.contains("nav-active")) {
-    //     navButton.classList.add("active-bar");
-    // } else {
-    //     navButton.classList.remove("active-bar");
-    // }
+    navButton.classList.toggle("active-bar");
 }
 
-// const closeNav = () => {
-//     navMenu.classList.remove("nav-active");
-// }
+const closeNav = () => {
+    navMenu.classList.remove("nav-active");
+    navButton.classList.remove("active-bar");
+}
+
+// For each link, loop through and add the closeNav functionality
+const iterateLinks = () => {
+    navLinks.forEach(link => {
+        link.addEventListener("click", closeNav);
+    })
+}
 
 // Event listeners
 scrollButton.addEventListener("click", scrollToAbout);
 navButton.addEventListener("click", toggleNav);
+
+// Envoke function
+iterateLinks();
+
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    var status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+    function success() {
+        contactForm.reset();
+        submitFormBtn.style = "display: none ";
+        // status.innerHTML = "Thanks!";
+    }
+
+    function error() {
+        console.log("Error");
+    }
+
+    // handle the form submission event
+    contactForm.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(contactForm);
+      ajax(contactForm.method, contactForm.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
